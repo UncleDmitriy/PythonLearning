b2b6acf63b32914822c59b905be676128fb80eb4
AkiraITDS <69007533+AkiraITDS@users.noreply.github.com>
2022-02-19 14:12:15 +0500

    Update and rename test.py to game.py

--- game.py
+++ game.py
@@ -2,8 +2,8 @@
 import pygame
 import random
 
-WIDTH = 360
-HEIGHT = 480
+WIDTH = 1080
+HEIGHT = 720
 FPS = 30
 
 # Задаем цвета
@@ -13,12 +13,27 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("images/kratos.gif").convert()
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+    def update(self):
+        self.rect.x += 5
+        if self.rect.left > WIDTH:
+            self.rect.right = 0
+
 # Создаем игру и окно
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 
 # Цикл игры
 running = True
@@ -32,9 +47,10 @@ while running:
             running = False
 
     # Обновлениее
-    
+    all_sprites.update()
     # Рендеринг
     screen.fill(BLACK)
+    all_sprites.draw(screen)
     # После отрисовки всего, переворачиваем экран
     pygame.display.flip()
 
